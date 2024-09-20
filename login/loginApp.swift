e5c4d413c8e48dcd0675e0c86b93f4b716133ef2
Mikhail Nikitin <jobles11@gmail.com>
2024-09-20 12:39:46 +0300

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by МИХАИЛ on 20.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
