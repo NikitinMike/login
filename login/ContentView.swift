0d9b3b3c60f6c2d2fb68d39a086aaf3827575270
Mikhail Nikitin <jobles11@gmail.com>
2024-09-24 09:36:57 +0300

    test http

--- login/ContentView.swift
+++ login/ContentView.swift
@@ -8,15 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text : String = "nobody buisness"
+    let url = "https://auth.olabank.ru/v1/security-oauth-server/" // "http://www.apple.com"
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Button("", systemImage: "brain", action: {getText(url: url)})
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.indigo)
+                .font(.system(size: 44))
+            Text(text)
         }
         .padding()
     }
+    
+    func getText(url : String) {
+        var request = URLRequest(url:URL(string: url)!)
+        request.setValue("iYOBA Client 2.0", forHTTPHeaderField: "user-agent")
+        let task = URLSession.shared.dataTask(with: request) {
+            (data, response, error) in guard let data = data else { return }
+            text = Date.now.formatted(date: .omitted, time: .standard) + ":" + (String(data: data, encoding: .utf8)!)
+        }
+        task.resume()
+        print(YOBAClient().getText(url: url))
+    }
+
 }
 
 #Preview {
--- login/YOBAClient.swift
+++ login/YOBAClient.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+import UIKit
+import SwiftUICore
+import WebKit
+
+struct YOBAClient {
+    @State var text = "nobody"
+    func getText(url : String) -> String {
+        var request = URLRequest(url:URL(string: url)!)
+        request.setValue("iYOBA Client 2.0", forHTTPHeaderField: "user-agent")
+        let task = URLSession.shared.dataTask(with: request) {
+            (data, response, error) in guard let data = data else { return }
+            text = Date.now.formatted(date: .omitted, time: .standard) + ":" + (String(data: data, encoding: .utf8)!)
+        }
+        task.resume()
+        print(text)
+        return text
+    }
+}
