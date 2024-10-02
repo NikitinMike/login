b358ce8094a6e72c1bc8fc7e560de7a0076ac417
Mikhail Nikitin <jobles11@gmail.com>
2024-10-02 12:09:25 +0300

    new sample

--- login/ContentView.swift
+++ login/ContentView.swift
@@ -6,36 +6,40 @@
 //
 
 import SwiftUI
+import RetroSwift
+import OkHttpClient
+import Foundation
 
 struct ContentView: View {
+    @State private var time : String = ""
     @State private var text : String = "nobody buisness"
     let url = "https://auth.olabank.ru/v1/security-oauth-server/" // "http://www.apple.com"
     
     var body: some View {
         VStack {
+            Text(time)
             Button("", systemImage: "brain", action: {getText(url: url)})
                 .imageScale(.large)
                 .foregroundStyle(.indigo)
                 .font(.system(size: 44))
-            Text(text)
+            Text(text.split(separator: "\",\"").joined(separator: "\n").replacing("\"", with: " "))
+                .multilineTextAlignment(.center)
         }
         .padding()
     }
-        
+         
     func getText(url : String) {
         var request = URLRequest(url:URL(string: url)!)
         request.setValue("iYOBA Client 2.0", forHTTPHeaderField: "user-agent")
         let task = URLSession.shared.dataTask(with: request) {
             (data, response, error) in guard let data = data else { return }
-            text = Date.now.formatted(date: .omitted, time: .standard) + " " + (String(data: data, encoding: .utf8)!)
-            text.replace("\",\"", with:"\n")
-            text.replace("\":\"", with:" : ")
+            time = Date.now.formatted(date: .omitted, time: .standard)
+            text = (String(data: data, encoding: .utf8)!)
+//            text.replace("\",\"", with:"\n")
+//            text.replace("\":\"", with:" : ")
         }
         task.resume()
         // print(YOBAClient().getText(url: url))
-        Task {
-            text = try! await Retro().getItem(url:url)
-        }
     }
     
 }
--- login/Retro.swift
+++ login/Retro.swift
@@ -7,13 +7,57 @@
 
 import RetroSwift
 import OkHttpClient
+import Foundation
 
 struct Retro {
-    typealias Item = String
-    func getItem(url: String) async throws -> Item {
-        //        @Header(name: "Content-Type") var contentType = "application/json"
+//    protocol HTTPClient {
+//        func execute<T: Decodable>(request: URLRequest) async throws -> T
+//    }
+
+//    typealias Item = String
+    func getItem(url: String) async throws -> String {
+//        @Header(name: "Content-Type") var contentType = "application/string"
         //        @Path(path: url) var path
+        let logger = SimpleLogger()
+        let client = OkHttpClient(logger: logger)
+        let retro = RetroSwift(client: client)
+        print(retro)
         @GET(baseURL: url) var request
-        return try! await RetroSwift(client: OkHttpClient()).execute(request: request)
+        print("===========")
+        print(request)
+        print("------------")
+        return try await retro.execute(request: request)
     }
+
+//    func task() {
+//        text = try await getItem(url:url)
+//        func getItem(url: String) async throws -> String {
+//    //        @Header(name: "Content-Type") var contentType = "application/string"
+//            //        @Path(path: url) var path
+//            let retro = RetroSwift(client: OkHttpClient(logger: SimpleLogger()))
+//            @GET(baseURL: url) var request
+//            return try await retro.execute(request: request)
+//        }
+//    }
+    
+//    var text : String
+    mutating func getText(url : String) {
+        var request = URLRequest(url:URL(string: url)!)
+        request.setValue("iYOBA Client 2.0", forHTTPHeaderField: "user-agent")
+        let task = URLSession.shared.dataTask(with: request) {
+            (data, response, error) in guard let data = data else { return }
+            var text = Date.now.formatted(date: .omitted, time: .standard) + " " + (String(data: data, encoding: .utf8)!)
+//            text.replace("\",\"", with:"\n")
+//            text.replace("\":\"", with:" : ")
+        }
+    //        task.resume()
+        // print(YOBAClient().getText(url: url))
+//        Task {
+//            print("===========")
+//            text = try! await Retro().getItem(url:url)
+//            print(text)
+//            print("------------")
+//        }
+    }
+    
 }
